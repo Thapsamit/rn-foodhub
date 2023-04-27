80953d2e2022f16d1b94057dfffc455f56d510fe
Thapsamit <thapliyalamit2001@gmail.com>
2023-04-27 11:30:24 +0530

    initial setup

---
+++ constants/theme.js
@@ -0,0 +1,17 @@
+const COLORS = {
+  primary: "rgba(191, 77, 28, 1)",
+  extraDark: "rgba(40, 42, 43, 1)",
+  lightDark: "rgba(40, 42, 43, 0.9)",
+  xLightDark: "rgba(40, 42, 43,0.7)",
+  white: "rgba(255, 255, 255, 0)",
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+export { COLORS, SIZES };
